7226b4966ebdd2b5e962bf195ece44e2d34b5cab
sschym <sschym@c77c9091-c757-4ec6-838d-c4341d657b51>
2008-08-29 13:27:06 +0000

    Changed watbal.f90 to solve tickets #17 and 19. Added new calculations of state variables when yu=0 and nlayers=0.
    git-svn-id: file:///home/sschyman/modelling/VOM/code/vom_bgc_repository/trunk@30 c77c9091-c757-4ec6-838d-c4341d657b51

--- VOM-code/watbal.f90
+++ VOM-code/watbal.f90
@@ -70,6 +70,8 @@ subroutine waterbalance(init)
   pcapnewvec(1:nlayersnew)= (-1.d0 + sunewvec(1:nlayersnew)**(-1.d0/mvg))**& ! (Out[54])
    (1.d0/nvg)/alphavg
   pcapnewvec(nlayersnew+1:M)=0.d0
+  kunsatnewvec=ksat*Sqrt(sunewvec)*(-1.d0+(1.d0-sunewvec**&    ! (3.14), (Out[55])
+   (1.d0/mvg))**mvg)**2.d0
   return
  endif
 
@@ -208,32 +210,41 @@ subroutine waterbalance(init)
 
  !* LENGTH OF TIME STEP
 
- dt=Min(1.d0,dtsu,dtmax) !dt must be small enough
+ if(nlayers.ge.1) then
+  dt=Min(1.d0,dtsu,dtmax) !dt must be small enough
 
- if(ys.le.zr) then
-  !* Testing for increase or decrease of yu:
-  yutarget=(-(cz*epsln) + dt*io + wc - delyu*epsln*(dt*sumdsutop + sumsutop - &
-   dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
-   nlayers*suvec(nlayers)))/(epsln*(-1 + dt*dsuvec(nlayers) + &
-   suvec(nlayers)))
- else
-  !* Making sure that Sqrt term is not negative  
-  dummy=-1.d0 
-  do while (dummy.lt.0.d0)
-   dummy=(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
+  if(ys.le.zr) then
+   !* Testing for increase or decrease of yu:
+   yutarget=(-(cz*epsln) + dt*io + wc - delyu*epsln*(dt*sumdsutop + sumsutop - &
+    dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
+    nlayers*suvec(nlayers)))/(epsln*(-1 + dt*dsuvec(nlayers) + &
+    suvec(nlayers)))
+  elseif(nlayers.ge.1) then
+   !* Making sure that Sqrt term is not negative  
+   dummy=-1.d0 
+   do while (dummy.lt.0.d0)
+    dummy=(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
+     dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
+     sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
+     nlayers*suvec(nlayers))**2))
+    dt=0.5d0*dt
+   enddo
+   yutarget=(delyu*epsln*(-(dt*sumdsutop) - sumsutop + dt*(-1 + &
+    nlayers)*dsuvec(nlayers) - suvec(nlayers) + nlayers*suvec(nlayers)) - &
+    Sqrt(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
     dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
     sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
-    nlayers*suvec(nlayers))**2))
-   dt=0.5d0*dt
-  enddo
-  yutarget=(delyu*epsln*(-(dt*sumdsutop) - sumsutop + dt*(-1 + &
-   nlayers)*dsuvec(nlayers) - suvec(nlayers) + nlayers*suvec(nlayers)) - &
-   Sqrt(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
-   dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
-   sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
-   nlayers*suvec(nlayers))**2)))/(2.*epsln*(-1 + dt*dsuvec(nlayers) + &
-   suvec(nlayers)))
- endif
+    nlayers*suvec(nlayers))**2)))/(2.*epsln*(-1 + dt*dsuvec(nlayers) + &
+    suvec(nlayers)))
+  else
+   yutarget=(delyu*epsln*(-(dt*sumdsutop) - sumsutop + dt*(-1 + &
+    nlayers)*dsuvec(nlayers) - suvec(nlayers) + nlayers*suvec(nlayers)) - &
+    Sqrt(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
+    dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
+    sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
+    nlayers*suvec(nlayers))**2)))/(2.*epsln*(-1 + dt*dsuvec(nlayers) + &
+    suvec(nlayers))) 
+  endif
 
 
 !!$ if(d.eq.62.and.h.eq.22.and.time.lt.50.d0) then
@@ -241,85 +252,85 @@ subroutine waterbalance(init)
 !!$ endif
 
 
- if(yutarget.gt.yu) then
-  if(yu.eq.Ceiling(yu/delyu)*delyu) then
-   yutarget=yu+delyu
-  else
-   yutarget=Ceiling(yu/delyu)*delyu
-  endif
-  if(yu.lt.cz-zr.and.yutarget.gt.cz-zr) then
-   yutarget=cz-zr
-  endif
-
- elseif(yutarget.lt.yu) then
-  if(yu.eq.Floor(yu/delyu)*delyu) then
-   yutarget=yu-delyu
-  else
-   yutarget=Floor(yu/delyu)*delyu
-  endif
-  if(yu.gt.cz-zr.and.yutarget.lt.cz-zr) then
-   yutarget=cz-zr
-  endif
- endif
-
-
- if(yutarget.gt.yu) then 
-  if(ys.lt.zr) then
-   if(yutarget.gt.cz) then
-    yutarget=cz                  !* PREVENTING NEGATIVE YS
+  if(yutarget.gt.yu) then
+   if(yu.eq.Ceiling(yu/delyu)*delyu) then
+    yutarget=yu+delyu
+   else
+    yutarget=Ceiling(yu/delyu)*delyu
    endif
-   dttarget=(-wc + epsln*(cz + yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
-    suvec(nlayers) - nlayers*suvec(nlayers))))/(io - &
-    epsln*yutarget*dsuvec(nlayers) + delyu*epsln*(-sumdsutop + (-1.d0 + &
-    nlayers)*dsuvec(nlayers)))
-  else
-   dttarget=(cz**2.d0*epsln + wc*zr - cz*(wc + epsln*zr) + &
-    epsln*yutarget*(yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
-    suvec(nlayers) - nlayers*suvec(nlayers))))/(cz*io - io*zr - &
-    epsln*yutarget*(yutarget*dsuvec(nlayers) + delyu*(sumdsutop + &
-    dsuvec(nlayers) - nlayers*dsuvec(nlayers))))
-  endif
- elseif(yutarget.lt.yu) then
-  if(ys.gt.zr) then
-   dttarget=(cz**2.d0*epsln + wc*zr - cz*(wc + epsln*zr) + &
-    epsln*yutarget*(yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
-    suvec(nlayers) - nlayers*suvec(nlayers))))/(cz*io - io*zr - &
-    epsln*yutarget*(yutarget*dsuvec(nlayers) + delyu*(sumdsutop + &
-    dsuvec(nlayers) - nlayers*dsuvec(nlayers))))
-  else
-   dttarget=(-wc + epsln*(cz + yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
-    suvec(nlayers) - nlayers*suvec(nlayers))))/(io - &
-    epsln*yutarget*dsuvec(nlayers) + delyu*epsln*(-sumdsutop + (-1.d0 + &
-    nlayers)*dsuvec(nlayers)))
+   if(yu.lt.cz-zr.and.yutarget.gt.cz-zr) then
+    yutarget=cz-zr
+   endif
+
+  elseif(yutarget.lt.yu) then
+   if(yu.eq.Floor(yu/delyu)*delyu) then
+    yutarget=yu-delyu
+   else
+    yutarget=Floor(yu/delyu)*delyu
+   endif
+   if(yu.gt.cz-zr.and.yutarget.lt.cz-zr) then
+    yutarget=cz-zr
+   endif
   endif
 
- else
-  dttarget=99999.d0
- endif
 
- if(dttarget.le.0.d0) then
-  dttarget=99999.d0
- endif
+  if(yutarget.gt.yu) then 
+   if(ys.lt.zr) then
+    if(yutarget.gt.cz) then
+     yutarget=cz                  !* PREVENTING NEGATIVE YS
+    endif
+    dttarget=(-wc + epsln*(cz + yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
+     suvec(nlayers) - nlayers*suvec(nlayers))))/(io - &
+     epsln*yutarget*dsuvec(nlayers) + delyu*epsln*(-sumdsutop + (-1.d0 + &
+     nlayers)*dsuvec(nlayers)))
+   else
+    dttarget=(cz**2.d0*epsln + wc*zr - cz*(wc + epsln*zr) + &
+     epsln*yutarget*(yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
+     suvec(nlayers) - nlayers*suvec(nlayers))))/(cz*io - io*zr - &
+     epsln*yutarget*(yutarget*dsuvec(nlayers) + delyu*(sumdsutop + &
+     dsuvec(nlayers) - nlayers*dsuvec(nlayers))))
+   endif
+  elseif(yutarget.lt.yu) then
+   if(ys.gt.zr) then
+    dttarget=(cz**2.d0*epsln + wc*zr - cz*(wc + epsln*zr) + &
+     epsln*yutarget*(yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
+     suvec(nlayers) - nlayers*suvec(nlayers))))/(cz*io - io*zr - &
+     epsln*yutarget*(yutarget*dsuvec(nlayers) + delyu*(sumdsutop + &
+     dsuvec(nlayers) - nlayers*dsuvec(nlayers))))
+   else
+    dttarget=(-wc + epsln*(cz + yutarget*(-1.d0 + suvec(nlayers)) + delyu*(sumsutop + &
+     suvec(nlayers) - nlayers*suvec(nlayers))))/(io - &
+     epsln*yutarget*dsuvec(nlayers) + delyu*epsln*(-sumdsutop + (-1.d0 + &
+     nlayers)*dsuvec(nlayers)))
+   endif
 
- dt=Max(0.d0,Min(dtsu,dttarget,dtmax))
+  else
+   dttarget=99999.d0
+  endif
 
- !*----- Calculating state variables at next time step----------------------- 
- !   
+  if(dttarget.le.0.d0) then
+   dttarget=99999.d0
+  endif
 
- if(ys.le.zr.and.yutarget.ge.cz-zr) then
-  yunew=(-(cz*epsln) + dt*io + wc - delyu*epsln*(dt*sumdsutop + sumsutop - &
-   dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
-   nlayers*suvec(nlayers)))/(epsln*(-1 + dt*dsuvec(nlayers) + &
-   suvec(nlayers))) 
- else
-  yunew=(delyu*epsln*(-(dt*sumdsutop) - sumsutop + dt*(-1 + &
-   nlayers)*dsuvec(nlayers) - suvec(nlayers) + nlayers*suvec(nlayers)) - &
-   Sqrt(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
-   dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
-   sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
-   nlayers*suvec(nlayers))**2)))/(2.*epsln*(-1 + dt*dsuvec(nlayers) + &
-   suvec(nlayers)))
- endif
+  dt=Max(0.d0,Min(dtsu,dttarget,dtmax))
+
+  !*----- Calculating state variables at next time step----------------------- 
+  !   
+
+  if(ys.le.zr.and.yutarget.ge.cz-zr) then
+   yunew=(-(cz*epsln) + dt*io + wc - delyu*epsln*(dt*sumdsutop + sumsutop - &
+    dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
+    nlayers*suvec(nlayers)))/(epsln*(-1 + dt*dsuvec(nlayers) + &
+    suvec(nlayers))) 
+  else
+   yunew=(delyu*epsln*(-(dt*sumdsutop) - sumsutop + dt*(-1 + &
+    nlayers)*dsuvec(nlayers) - suvec(nlayers) + nlayers*suvec(nlayers)) - &
+    Sqrt(epsln*(4*(-(cz*epsln) + dt*io + wc)*(cz - zr)*(-1 + &
+    dt*dsuvec(nlayers) + suvec(nlayers)) + delyu**2*epsln*(dt*sumdsutop + &
+    sumsutop - dt*(-1 + nlayers)*dsuvec(nlayers) + suvec(nlayers) - &
+    nlayers*suvec(nlayers))**2)))/(2.*epsln*(-1 + dt*dsuvec(nlayers) + &
+    suvec(nlayers)))
+  endif
 
 
 
@@ -338,48 +349,94 @@ subroutine waterbalance(init)
 
 
 
- !*Rounding layers of less than 0.001 mm away from delyu
- if(Abs(yunew - nlayers*delyu).lt.1.e-9) then
-  yunew=nlayers*delyu
- endif
- if(Abs(yunew - (nlayers-1)*delyu).lt.1.e-9) then
-  yunew=(nlayers-1)*delyu
- endif
+  !*Rounding layers of less than 0.001 mm away from delyu
+  if(Abs(yunew - nlayers*delyu).lt.1.d-9) then
+   yunew=nlayers*delyu
+  endif
+  if(Abs(yunew - (nlayers-1)*delyu).lt.1.d-9) then
+   yunew=(nlayers-1)*delyu
+  endif
 
-!* Calculating other state variables at next time step
- if(yunew.ge.cz-zr) then
-  omgunew=1.d0
- else
+  !* Calculating other state variables at next time step
+  if(yunew.ge.cz-zr) then
+   omgunew=1.d0
+  else
+   omgunew=yunew/(cz - zr)
+  endif
+
+  omgonew=1.d0-omgunew
+  ysnew=cz-omgunew*yunew
+  nlayersnew=Ceiling(yunew/delyu) ! The bottom layer is smaller than or equal to delyu
+  delyunewvec(:)=delyu
+  if(nlayersnew.ge.1) then 
+   delyunewvec(nlayersnew)=yunew - (nlayersnew - 1)*delyu
+  endif
+
+  if(nlayers.ge.1) then
+   sunewvec(1:nlayers)=suvec(1:nlayers)+dt*dsuvec(1:nlayers)
+   pcapnewvec(1:nlayers)= (-1.d0 + sunewvec(1:nlayers)**(-1.d0/mvg))**& ! (Out[54])
+    (1.d0/nvg)/alphavg
+   sunewvec(nlayersnew+1:M)= 1.d0
+   pcapvec(nlayersnew+1:M)=0.d0
+
+  else
+   sunewvec=1.d0
+   pcapnewvec=0.d0
+  endif
+
+  !*ADAPTING SOIL MOISTURE TO NEW NUMBER OF LAYERS
+  if(nlayersnew.gt.nlayers) then
+   sunewvec(nlayersnew)=sunewvec(nlayers)
+   pcapnewvec(nlayersnew)= (-1.d0 + sunewvec(nlayersnew)**(-1.d0/mvg))**& ! (Out[54])
+    (1.d0/nvg)/alphavg
+  endif
+
+ else  !*If nlayers=0 and io<0, then make yu increase to yu=delyuvec or any value small enough to fit within dt
+  if(io.lt.0.d0) then
+   yunew=2.d0*delyu
+   do while(dt.gt.Min(dtsu,dttarget,dtmax))  !*yunew must be small enough to be reached within dtmax
+    yunew=yunew/2.d0
+    dt=(-1.d0*(1.d0*wc - 1.d0*epsln*(cz - (1.d0*yunew**2.d0)/&
+     (cz - 1.d0*zr) + (yunew**2.d0*(1.d0/(1.d0 + 0.5d0**nvg*&
+     (alphavg*yunew)**nvg))**mvg)/(cz - 1.d0*zr))))/io
+   enddo
+  else
+   dt=Max(0.d0,Min(dtsu,dttarget,dtmax))
+   yunew=yu
+  endif
+
+  !*Rounding layers of less than 0.001 mm away from delyu
+  if(yunew.lt.1.d-9) then
+   yunew=0.d0
+  endif
+
+  !* Calculating other state variables at next time step
   omgunew=yunew/(cz - zr)
+  omgonew=1.d0-omgunew
+  ysnew=cz-omgunew*yunew
+  nlayersnew=Ceiling(yunew/delyu) ! The bottom layer is smaller than or equal to delyu
+  delyunewvec(:)=delyu
+  if(nlayersnew.eq.1) then 
+   delyunewvec(nlayersnew)=yunew
+  endif
+
+  if(nlayersnew.eq.1) then
+   sunewvec(1)=(1.d0/(1.d0+0.5d0**nvg*(alphavg*yunew)**nvg))**mvg  ! The soil moisture left behind is at field capacity
+   pcapnewvec(1)= (-1.d0 + sunewvec(1)**(-1.d0/mvg))**& ! (Out[54])
+    (1.d0/nvg)/alphavg
+   sunewvec(nlayersnew+1:M)= 1.d0
+   pcapvec(nlayersnew+1:M)=0.d0
+
+  else
+   sunewvec=1.d0
+   pcapnewvec=0.d0
+  endif
+
+
+
+
+
  endif
-
- omgonew=1.d0-omgunew
- ysnew=cz-omgunew*yunew
- nlayersnew=Ceiling(yunew/delyu) ! The bottom layer is smaller than or equal to delyu
- delyunewvec(:)=delyu
- if(nlayersnew.ge.1) then 
-  delyunewvec(nlayersnew)=yunew - (nlayersnew - 1)*delyu
- endif
-
- if(nlayers.ge.1) then
-  sunewvec(1:nlayers)=suvec(1:nlayers)+dt*dsuvec(1:nlayers)
-  pcapnewvec(1:nlayers)= (-1.d0 + sunewvec(1:nlayers)**(-1.d0/mvg))**& ! (Out[54])
-   (1.d0/nvg)/alphavg
-  sunewvec(nlayersnew+1:M)= 1.d0
-  pcapvec(nlayersnew+1:M)=0.d0
-
- else
-  sunewvec=1.d0
-  pcapnewvec=0.d0
- endif
-
- !*ADAPTING SOIL MOISTURE TO NEW NUMBER OF LAYERS
- if(nlayersnew.gt.nlayers) then
-  sunewvec(nlayersnew)=sunewvec(nlayers)
-  pcapnewvec(nlayersnew)= (-1.d0 + sunewvec(nlayersnew)**(-1.d0/mvg))**& ! (Out[54])
-   (1.d0/nvg)/alphavg
- endif
-
 
 
  kunsatnewvec=ksat*Sqrt(sunewvec)*(-1.d0+(1.d0-sunewvec**&    ! (3.14), (Out[55])
