1055908a2117f6c87b6c4522e6d51c4c7c24ddda
Remko Nijzink <remko.nijzink@list.lu>
2018-12-27 13:16:16 +0100

    cleaned up for pr

--- VOM_Fortran/VOM-code/transpmodel.f90
+++ VOM_Fortran/VOM-code/transpmodel.f90
@@ -298,10 +298,6 @@
 
 !     * get timeseries of vegetation cover
 
-      if( i_read_pc == 1) then
-         call vom_get_perc_cov()
-      end if
-
 
       return
       end subroutine transpmodel_init_once
@@ -424,7 +420,7 @@
      &                    i_rrootm, i_rsurfmin, i_rsurf_, i_rootrad,   &
      &                    i_prootmg, i_growthmax, i_incrcovg,          &
      &                    i_incrjmax,                                  &
-     &                    i_firstyear,i_lastyear, i_write_h, i_read_pc,&
+     &                    i_firstyear,i_lastyear, i_write_h,&
      &                    i_inputpath, i_outputpath,                   &
      &                    o_lambdagf, o_wsgexp, o_lambdatf, o_wstexp,  &
      &                    o_pct, o_rtdepth, o_mdstore, o_rgdepth
@@ -654,58 +650,7 @@
       return
       end subroutine vom_get_soilprofile
 
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-!*-----READING TIMESERIES OF VEGETATION COVER---------------------------
 
-      subroutine vom_get_perc_cov ()
-      use vom_vegwat_mod
-      implicit none
-
-      INTEGER :: iostat, i, j
-      INTEGER, ALLOCATABLE :: fyear_pc(:)    ! Year for each day
-      INTEGER, ALLOCATABLE :: fmonth_pc(:)   ! Month for each day
-      INTEGER, ALLOCATABLE :: fday_pc(:)     ! Day of month
-      INTEGER, ALLOCATABLE :: dayyear_pc(:)  ! Day of year
-
-
-
-     allocate(dayyear_pc( c_maxday    )   )
-     allocate(fday_pc( c_maxday    )   )
-     allocate(fmonth_pc( c_maxday    )   )
-     allocate(fyear_pc( c_maxday    )   )     
-     allocate(perc_cov_veg( c_maxday    )   )
-
-
-      open(kfile_perc_cov, FILE=trim(adjustl(i_inputpath))// &
-           trim(adjustl(sfile_perc_cov)),                  &
-     &                        STATUS='old', IOSTAT=iostat)
-      if (iostat .eq. 0) then
-         do i = 1, c_maxday
-          read(kfile_perc_cov,'(4i8,f8.2)') dayyear_pc(i), fday_pc(i), &
-     &      fmonth_pc(i), fyear_pc(i), perc_cov_veg(i)
-        enddo
-      end if 
-      close(kfile_perc_cov)
-
-      !check if timeseries match
-
-     if( (fyear_pc(1) .ne. fyear(1)) .or. &
-         (fday_pc(1) .ne. fday(1)) .or. &
-         (fmonth_pc(1) .ne. fmonth(1)) ) then
-     stop 'startdate of perc_cov doesnot match with dailyweather'
-     end if
-
-     if( (fyear_pc(c_maxday) .ne. fyear(c_maxday)) .or. &
-         (fday_pc(c_maxday) .ne. fday(c_maxday)) .or. &
-         (fmonth_pc(c_maxday) .ne. fmonth(c_maxday)) ) then
-     stop 'enddate of perc_cov doesnot match with dailyweather'
-     end if
-
-
-      return
-      end subroutine vom_get_perc_cov
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -956,18 +901,11 @@
       jmax25t_d(2) = 0.0003d0
       jmax25g_d(2) = 0.0003d0
       c_pcgmin     = 0.02d0             ! minimum grass pc; initial point for growth
-      if(i_read_pc == 1) then
-         pcg_d(:) = perc_cov_veg( 1 )
-         !adjust value if perennial + seasonal > 1
-         if( (pcg_d(1) + o_pct) .gt. 1.0) then
-            pcg_d(:) = 1.d0 - o_pct
-         end if
-
-      else       
+      
          pcg_d(2)     = MIN(1.d0 - o_pct, c_pcgmin)
          pcg_d(:)     = pcg_d(2) + (/-i_incrcovg,0.0d0,i_incrcovg/)  ! vector with values varying by 1%
          pcg_d(3)     = MIN(MAX(c_pcgmin, pcg_d(3)), 1.d0 - o_pct)
-      end if
+
 
       rootlim(:,:) = 0.d0
 
@@ -1027,19 +965,10 @@
       jmax25t_d(:) = MAX(jmax25t_d(:), 50.0d-6)
       jmax25g_d(:) = jmax25g_d(2) * (/1.0d0-i_incrjmax,1.0d0,1.0d0+i_incrjmax/)
       jmax25g_d(:) = MAX(jmax25g_d(:), 50.0d-6)
-      if( i_read_pc == 1) then   
-         pcg_d(:) = perc_cov_veg(nday)
+      pcg_d(:)     = pcg_d(2) + (/-i_incrcovg,0.0d0,i_incrcovg/)  ! perc. change grass cover
+      pcg_d(:)     = MAX(pcg_d(:), 0.d0)
+      pcg_d(3)     = MIN(MAX(c_pcgmin, pcg_d(3)), 1.d0 - o_pct)
 
-         !adjust value if perennial + seasonal > 1
-         if( (pcg_d(1) + o_pct) .gt. 1.0) then
-            pcg_d(:) = 1.d0 - o_pct
-         end if
-
-      else
-         pcg_d(:)     = pcg_d(2) + (/-i_incrcovg,0.0d0,i_incrcovg/)  ! perc. change grass cover
-         pcg_d(:)     = MAX(pcg_d(:), 0.d0)
-         pcg_d(3)     = MIN(MAX(c_pcgmin, pcg_d(3)), 1.d0 - o_pct)
-      end if
 
 
 !     * (3.38) foliage turnover costs, assuming LAI/pc of 2.5
