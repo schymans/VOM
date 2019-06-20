1ba4dce44d8d074572b13a292ff831ab95fb2be9
Remko Nijzink <remko.nijzink@list.lu>
2019-06-20 17:55:03 +0200

    changed python-script for checking

--- VOM_Fortran/VOM_test/ref_test/check_outcomes.py
+++ VOM_Fortran/VOM_test/ref_test/check_outcomes.py
@@ -9,7 +9,7 @@ print("Checking for differences in results...")
 
 
 results_file = 'output/results_daily.txt'
-reference_file = '../ref_test/resultsdaily_best.txt'
+reference_file = '../ref_test/results_daily.txt'
 
 results = pd.read_csv(results_file, skiprows = 0, dtype=np.float64, delimiter=r"\s+")
 reference = pd.read_csv(reference_file, skiprows = 0, dtype=np.float64, delimiter=r"\s+")
@@ -78,11 +78,18 @@ succes[4] = check_results(results["asst"], reference["asst"], "asst")
 succes[5] = check_results(results["jmax25t"], reference["jmax25t"], "jmax25t")
 succes[6] = check_results(results["jmax25g"], reference["jmax25g"], "jmax25g")
 
+print("")
+print("===================================================")
+print("")
+print("Result:")
+print(str(np.sum(succes)) + "tests out of 6 passed")
+
 if( np.sum(succes) > 3):
-    print("More then 3 conclusions valid")
+    print("More then 3 conclusions still valid")
     sys.exit(0)
 else:
     print("Less then 3 conclusions valid")
+    print("Throwing an error, good luck next time!")
     sys.exit(1)
 
 
