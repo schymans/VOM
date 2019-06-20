ca1c09e7e9dbf4731767249c810f3b9b0b77aa2d
Remko Nijzink <remko.nijzink@list.lu>
2019-06-20 17:27:20 +0200

    changed checks

--- VOM_Fortran/VOM_test/ref_test/check_outcomes.py
+++ VOM_Fortran/VOM_test/ref_test/check_outcomes.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import csv
+import os
 import sys
 from termcolor import colored
 
@@ -9,7 +10,7 @@ print("Checking for differences in results...")
 
 
 results_file = 'output/results_daily.txt'
-reference_file = '../ref_test/results_daily.txt'
+reference_file = '../ref_test/resultsdaily_best.txt'
 
 results = pd.read_csv(results_file, skiprows = 0, dtype=np.float64, delimiter=r"\s+")
 reference = pd.read_csv(reference_file, skiprows = 0, dtype=np.float64, delimiter=r"\s+")
@@ -43,10 +44,10 @@ reference.index = index
 ########################################
 #make mean yearly values etmt
 
-def check_results(var, varname):
+def check_results(var, var_ref, varname):
 
     results = var.resample('Y').mean()   
-    reference = var.resample('Y').mean()   
+    reference = var_ref.resample('Y').mean()   
 
     ann_results = np.mean(results)
     ann_reference = np.mean(reference)
@@ -58,26 +59,32 @@ def check_results(var, varname):
         print(diff)
         print("Conclusions are not valid any more")
         print(colored("TEST FAILED", "red"))
-        sys.exit(1)
+        success = False
+
 
     if np.abs(diff) < 10:
         print("Mean annual " + varname + " changed less than 10%:")
         print(diff)
         print("Conclusions are still valid")
         print(colored("TEST PASSED", "green"))
-        sys.exit(0)
+        success = True
+    return success
 
+succes = np.ones((7), dtype=bool)
+succes[0] = check_results(results["etmt"], reference["etmt"],"etmt", )
+succes[1] = check_results(results["etmg"], reference["etmg"], "etmg")
+succes[2] = check_results(results["esoil"], reference["esoil"], "esoil")
+succes[3] = check_results(results["assg"], reference["assg"], "assg")
+succes[4] = check_results(results["asst"], reference["asst"], "asst")
+succes[5] = check_results(results["jmax25t"], reference["jmax25t"], "jmax25t")
+succes[6] = check_results(results["jmax25g"], reference["jmax25g"], "jmax25g")
 
-
-check_results(results["etmt"],"etmt" )
-check_results(results["etmg"], "etmg")
-check_results(results["esoil"], "esoil")
-check_results(results["assg"], "assg" )
-check_results(results["asst"], "asst")
-check_results(results["jmax25t"], "jmax25t" )
-check_results(results["jmax25g"], "jmax25g")
-
-
+if( np.sum(succes) > 3):
+    print("More then 3 conclusions valid")
+    sys.exit(0)
+else:
+    print("Less then 3 conclusions valid")
+    sys.exit(1)
 
 
 
